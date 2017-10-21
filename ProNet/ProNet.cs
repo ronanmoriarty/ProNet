6761365c9bd8f8392e01d157bb0f008fd24a690b
Ronan Moriarty <ronan.moriarty@hotmail.co.uk>
2017-10-21 15:30:07 +0100

    Validate xml

--- ProNet/ProNet.cs
+++ ProNet/ProNet.cs
@@ -1,15 +1,38 @@
 using System;
 using System.IO;
+using System.Xml;
+using System.Xml.Schema;
 
 namespace ProNet
 {
     public class ProNet : IProNet
     {
+        private readonly string _filename;
+
         public ProNet(string filename)
         {
             if (!File.Exists(filename))
             {
                 throw new ArgumentException($"File {filename} was not found");
+            }
+            _filename = filename;
+
+            var xmlReaderSettings = new XmlReaderSettings();
+            var proNetSchemaPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ProNet.xsd");
+            xmlReaderSettings.Schemas.Add(null ,proNetSchemaPath);
+            xmlReaderSettings.ValidationType = ValidationType.Schema;
+            xmlReaderSettings.ValidationEventHandler += HandleValidationEvent;
+
+            var xmlReader = XmlReader.Create(filename, xmlReaderSettings);
+
+            while (xmlReader.Read()) { }
+        }
+
+        void HandleValidationEvent(object sender, ValidationEventArgs e)
+        {
+            if (e.Severity == XmlSeverityType.Error)
+            {
+                throw new ArgumentException($"File {_filename} is not a valid ProNet data file");
             }
         }
 
